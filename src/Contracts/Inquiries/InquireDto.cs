ef512dcf44efe669e0607cd92eea2116ed9a7e97
Birchman <jakubbrzoskowski@gmail.com>
2022-12-08 14:33:22 +0100

    Add PaginationDto

--- src/Contracts/Inquiries/InquireDto.cs
+++ src/Contracts/Inquiries/InquireDto.cs
@@ -5,9 +5,9 @@ namespace Contracts.Inquiries;
 public class InquireDto
 {
     public Guid Id { get; set; }
-    public Guid? UserId { get; private init; }
-    public PersonalDataDto? PersonalData { get; private init; }
-    public int MoneyInSmallestUnit { get; private init; }
-    public int NumberOfInstallments { get; private init; }
-    public DateTime CreationTime { get; private init; }
+    public Guid? UserId { get; set; }
+    public PersonalDataDto? PersonalData { get; set; }
+    public int MoneyInSmallestUnit { get; set; }
+    public int NumberOfInstallments { get; set; }
+    public DateTime CreationTime { get; set; }
 }
