77582542b624ae99bafff79cebdb85e449f94e10
Michal Boguszewski <michal1842001@gmail.com>
2023-01-17 20:09:14 +0100

    Create 2 simple tests for creating inquire

---
+++ tests/IntegrationTests/Api/Inquiries/CreateInquireTests.cs
@@ -0,0 +1,55 @@
+using System.Net;
+using Contracts.Api.Inquiries;
+using Contracts.Shared.Users;
+using FastEndpoints;
+using FluentAssertions;
+using Services.Endpoints.Api.Inquiries;
+using Xunit;
+
+namespace IntegrationTests.Api.Inquiries;
+
+public class CreateInquireTests : TestBase
+{
+    public CreateInquireTests(ApiWebFactory apiWebFactory) : base(apiWebFactory)
+    { }
+
+    [Fact]
+    public async Task Inquire_is_created()
+    {
+        var result = await ApiClient.POSTAsync<PostCreateAnonymousInquireEndpoint, PostCreateAnonymousInquire>(new PostCreateAnonymousInquire()
+        {
+            MoneyInSmallestUnit = 1000,
+            NumberOfInstallments = 12,
+            PersonalData = new()
+            {
+                FirstName = "name",
+                GovernmentId = "00000000000",
+                GovernmentIdType = GovernmentIdTypeDto.Pesel,
+                JobType = JobTypeDto.SomeJobType,
+                LastName = "lastname",
+            },
+        });
+
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task Validation_fails_because_of_invalid_pesel()
+    {
+        var result = await ApiClient.POSTAsync<PostCreateAnonymousInquireEndpoint, PostCreateAnonymousInquire>(new PostCreateAnonymousInquire()
+        {
+            MoneyInSmallestUnit = 1000,
+            NumberOfInstallments = 12,
+            PersonalData = new()
+            {
+                FirstName = "name",
+                GovernmentId = "0000000000",
+                GovernmentIdType = GovernmentIdTypeDto.Pesel,
+                JobType = JobTypeDto.SomeJobType,
+                LastName = "lastname",
+            },
+        });
+
+        result.StatusCode.Should().Be((HttpStatusCode)400);
+    }
+}
