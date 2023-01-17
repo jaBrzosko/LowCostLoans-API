749dba7af240cc3c5cc433d9846b3a00a9dba3d1
Michal Boguszewski <michal1842001@gmail.com>
2023-01-17 17:47:29 +0100

    Make personal data not nullable

--- src/Contracts/Frontend/Inquiries/InquireDto.cs
+++ src/Contracts/Frontend/Inquiries/InquireDto.cs
@@ -5,7 +5,7 @@ namespace Contracts.Frontend.Inquiries;
 public class InquireDto
 {
     public Guid Id { get; set; }
-    public PersonalDataDto? PersonalData { get; set; }
+    public PersonalDataDto PersonalData { get; set; }
     public int MoneyInSmallestUnit { get; set; }
     public int NumberOfInstallments { get; set; }
     public DateTime CreationTime { get; set; }
--- src/Domain/Inquiries/Inquire.cs
+++ src/Domain/Inquiries/Inquire.cs
@@ -5,14 +5,14 @@ namespace Domain.Inquiries;
 public class Inquire
 {
     public Guid Id { get; private init; }
-    public PersonalData? PersonalData { get; private init; }
+    public PersonalData PersonalData { get; private init; }
     public int MoneyInSmallestUnit { get; private init; }
     public int NumberOfInstallments { get; private init; }
     public DateTime CreationTime { get; private init; }
 
-    public Inquire(PersonalData? personalData, int moneyInSmallestUnit, int numberOfInstallments)
+    public Inquire(PersonalData personalData, int moneyInSmallestUnit, int numberOfInstallments)
     {
-        Validate(personalData, moneyInSmallestUnit, numberOfInstallments);
+        Validate(moneyInSmallestUnit, numberOfInstallments);
         
         Id = Guid.NewGuid();
         PersonalData = personalData;
@@ -24,13 +24,8 @@ public class Inquire
     private Inquire() 
     { }
 
-    private static void Validate(PersonalData? personalData, int moneyInSmallestUnit, int numberOfInstallments)
+    private static void Validate(int moneyInSmallestUnit, int numberOfInstallments)
     {
-        if (personalData is null)
-        {
-            throw new ArgumentException("PersonalData can not be null");
-        }
-
         if (moneyInSmallestUnit <= 0)
         {
             throw new ArgumentException("Money has to be positive");
