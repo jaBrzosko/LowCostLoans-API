43718ca4683b3b750d9e438ec7f70a5d9784e2ff
Michal Boguszewski <michal1842001@gmail.com>
2023-01-18 20:33:12 +0100

    Finish testing creating offer

--- tests/IntegrationTests/Frontend/Offers/CreatingOffersTests.cs
+++ tests/IntegrationTests/Frontend/Offers/CreatingOffersTests.cs
@@ -1,7 +1,13 @@
 using System.Net;
+using Contracts.Api.Inquiries;
+using Contracts.Api.Offers;
+using Contracts.Common;
 using Contracts.Frontend.Offers;
+using Contracts.Shared.Users;
 using FastEndpoints;
 using FluentAssertions;
+using Services.Endpoints.Api.Inquiries;
+using Services.Endpoints.Api.Offers;
 using Services.Endpoints.Frontend.Offers;
 using Xunit;
 
@@ -15,12 +21,65 @@ public class OffersCreated : TestBaseWithFrontendAuthentication
     [Fact]
     public async Task Test()
     {
-        await CreateOfferTemplates();
+        await CreateOfferTemplatesAsync();
+        var inquireId = await CreateInquireAndEnsureCorrectnessAsync();
+        var expectedOfferList = new OfferListDto()
+        {
+            InquireId = inquireId,
+            MoneyInSmallestUnit = 1000,
+            NumberOfInstallments = 8,
+            Offers = new()
+            {
+                new()
+                {
+                    InterestRate = 12,
+                },
+                new()
+                {
+                    InterestRate = 12,
+                },
+            },
+        };
+
+        var actualOfferList = await ApiClient.GETAsync<GetOffersByInquireIdEndpoint, GetOffersByInquireId, OfferListDto?>(
+            new GetOffersByInquireId()
+            {
+                Id = inquireId,
+            });
+
+        actualOfferList.response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        actualOfferList.result.Should().BeEquivalentTo(expectedOfferList, options => options.Excluding(p => p.Offers));
+        actualOfferList.result.Offers.Should().BeEquivalentTo(expectedOfferList.Offers,
+            options => options.Excluding(p => p.Id).Excluding(p => p.CreationTime));
     }
 
-    private async Task CreateOfferTemplates()
+    private async Task<Guid> CreateInquireAndEnsureCorrectnessAsync()
     {
-        var offerTemplateResult = await FrontendClient.POSTAsync<PostCreateOfferTemplateEndpoint, PostCreateOfferTemplate>(
+        var result = await ApiClient.POSTAsync<PostCreateAnonymousInquireEndpoint, PostCreateAnonymousInquire, PostResponseWithIdDto>(
+            new PostCreateAnonymousInquire()
+            {
+                MoneyInSmallestUnit = 1000,
+                NumberOfInstallments = 8,
+                PersonalData = new PersonalDataDto()
+                {
+                    FirstName = "name",
+                    LastName = "lastname",
+                    GovernmentId = "00000000000",
+                    GovernmentIdType = GovernmentIdTypeDto.Pesel,
+                    JobType = JobTypeDto.SomeJobType,
+                },
+            });
+
+        result.response.StatusCode.Should().Be(HttpStatusCode.OK);
+        result.result.Id.Should().NotBeEmpty();
+
+        return result.result.Id;
+    }
+
+    private async Task CreateOfferTemplatesAsync()
+    {
+        var result = await FrontendClient.POSTAsync<PostCreateOfferTemplateEndpoint, PostCreateOfferTemplate>(
             new PostCreateOfferTemplate()
             {
                 InteresetRate = 12,
@@ -30,9 +89,9 @@ public class OffersCreated : TestBaseWithFrontendAuthentication
                 MinimumMoneyInSmallestUnit = 100,
             });
 
-        offerTemplateResult!.StatusCode.Should().Be(HttpStatusCode.OK);
+        result!.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        offerTemplateResult = await FrontendClient.POSTAsync<PostCreateOfferTemplateEndpoint, PostCreateOfferTemplate>(
+        result = await FrontendClient.POSTAsync<PostCreateOfferTemplateEndpoint, PostCreateOfferTemplate>(
             new PostCreateOfferTemplate()
             {
                 InteresetRate = 12,
@@ -42,6 +101,6 @@ public class OffersCreated : TestBaseWithFrontendAuthentication
                 MinimumMoneyInSmallestUnit = 100,
             });
 
-        offerTemplateResult!.StatusCode.Should().Be(HttpStatusCode.OK);
+        result!.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 }
